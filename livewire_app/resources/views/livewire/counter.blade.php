fce1b1dbbc40d8f2db1c496e484e48221ba1797b
AretuzaRaquely <aretuzaraquely@gmail.com>
2023-07-12 17:46:05 -0300

    aula 12/07

---
+++ livewire_app/resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div>
+    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+    
+</div>
